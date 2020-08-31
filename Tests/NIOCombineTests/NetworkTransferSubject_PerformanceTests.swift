b023f708a85fb6f72884dcd8e3e436c3d62624e2
月光虹 <iris@random.town>
2020-08-31 12:12:36 +0200

    WIP: Stub performance specification

---
+++ Tests/NIOCombineTests/NetworkTransferSubject_PerformanceTests.swift
@@ -0,0 +1,34 @@
+import Foundation
+import Combine
+import NIO
+import XCTest
+
+@testable import NIOCombine
+
+final class NetworkTransferSubject_PerformanceTests: XCTestCase {
+
+    ///
+
+    func test_Latency_ofWriting() throws {
+        throw XCTSkip("TODO: Reading speed should be predictable.")
+    }
+
+    ///
+
+    func test_Latency_ofReading() throws {
+        throw XCTSkip("TODO: Writing speed should be predictable as well.")
+    }
+
+    ///
+
+    func test_OverallThroughput_usingConstantPredictablePackets() throws {
+        throw XCTSkip("TODO: Overall transfer speed should be predictable in an organised environment.")
+    }
+
+    ///
+
+    func test_OverallThroughput_usingChaoticallySizedPackets() throws {
+        throw XCTSkip("TODO: Overall transfer speed should be predictable even among chaos.")
+    }
+
+}
